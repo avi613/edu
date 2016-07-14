1df80375f2463447adacb8f0229da63aecb67049
avi613 <avi.elkharrat@gmail.com>
2016-07-15 00:04:02 +0200

    init random walk

---
+++ dla/random-walk-1D/src/test/java/avi/edu/dla/rand/RandomGeneratorTest.java
@@ -0,0 +1,14 @@
+package avi.edu.dla.rand;
+
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class RandomGeneratorTest {
+    private RandomGenerator randomGenerator = new RandomGenerator();
+
+    @Test
+    public void it_should_generate_random_ints_between_0_and_1() {
+        assertThat(randomGenerator.nextMove()).isBetween(0, 1);
+    }
+}
