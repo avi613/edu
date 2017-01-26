4cf27a20532918748f79d86f6710b7aa14aa4698
avi613 <avi.elkharrat@gmail.com>
2017-01-26 22:56:52 +0100

    hey little embedded

--- poc-mongo/src/test/java/hello/CustomerRepositoryIT.java
+++ poc-mongo/src/test/java/hello/CustomerRepositoryIT.java
@@ -15,7 +15,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 @RunWith(SpringRunner.class)
 @EnableAutoConfiguration
-public class CustomerRepositoryTest {
+public class CustomerRepositoryIT {
     @Autowired
     private CustomerRepository repository;
 
